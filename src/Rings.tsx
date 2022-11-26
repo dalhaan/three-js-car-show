17e0538636216e7a43b182e42bf7f6732e83ee1e
Dallan Freemantle <dallan@alphero.com>
2022-11-27 02:04:54 +1300

    Update rings

--- src/Rings.tsx
+++ src/Rings.tsx
@@ -1,13 +1,50 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { BufferGeometry, Material, Mesh } from "three";
-import { useControls } from "leva";
+import {
+  BufferGeometry,
+  Color,
+  Material,
+  Mesh,
+  MeshStandardMaterial,
+} from "three";
+import { useControls, folder } from "leva";
 
 type MeshType = Mesh<BufferGeometry, Material | Material[]> | null;
 
 export function Rings() {
-  const { noRings } = useControls({
-    noRings: 14,
+  const {
+    noRings,
+    ringsScaleFalloff,
+    ringsIntensityFalloff,
+    ringsIntensityMul,
+  } = useControls({
+    rings: folder({
+      noRings: {
+        label: "count",
+        value: 14,
+      },
+      ringsScaleFalloff: {
+        label: "Scale falloff",
+        value: 0.04,
+        min: 0,
+        max: 0.1,
+        step: 0.01,
+      },
+      ringsIntensityFalloff: {
+        label: "Intensity falloff",
+        value: 10,
+        min: 0,
+        max: 50,
+        step: 0.1,
+      },
+      ringsIntensityMul: {
+        label: "Intensity Mul",
+        value: 0.5,
+        min: 0,
+        max: 1,
+        step: 0.01,
+      },
+    }),
   });
 
   const itemsRef = useRef<MeshType[]>([]);
@@ -17,8 +54,40 @@ export function Rings() {
       const mesh = itemsRef.current[i];
 
       if (mesh) {
+        // Update rings position
         const z = (i - noRings / 2) * 3.5;
         mesh.position.set(0, 0, -z);
+
+        // Update ring scale falloff
+        const dist = Math.abs(z);
+        mesh.scale.set(
+          1 - dist * ringsScaleFalloff,
+          1 - dist * ringsScaleFalloff,
+          1 - dist * ringsScaleFalloff
+        );
+
+        // Set ring colours
+        if (mesh.material instanceof MeshStandardMaterial) {
+          // Colour falloff
+          let colourScale = 1;
+          if (dist > 2) {
+            colourScale =
+              1 -
+              (Math.min(dist, ringsIntensityFalloff + 2) - 2) /
+                ringsIntensityFalloff;
+          }
+          colourScale *= ringsIntensityMul;
+
+          if (i % 2 === 1) {
+            mesh.material.emissive = new Color(6, 0.15, 0.7).multiplyScalar(
+              colourScale
+            );
+          } else {
+            mesh.material.emissive = new Color(0.1, 0.7, 3).multiplyScalar(
+              colourScale
+            );
+          }
+        }
       }
     }
   });
